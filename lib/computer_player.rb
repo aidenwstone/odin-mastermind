889849f171562daa29aacee546107f19a9507718
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-07-28 17:56:33 -0400

    Implement prompt_for_name method

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -3,4 +3,9 @@
 require_relative 'player'
 
 class ComputerPlayer < Player
+  POSSIBLE_NAMES = ['Moyer the Destroyer', 'Bad Chad', 'Baron von Cavitus', 'Dr. McFisto', 'Mr. Nemesis'].freeze
+
+  def prompt_for_name
+    POSSIBLE_NAMES.sample
+  end
 end
--- lib/human_player.rb
+++ lib/human_player.rb
@@ -3,4 +3,8 @@
 require_relative 'player'
 
 class HumanPlayer < Player
+  def prompt_for_name
+    print "Please enter your name (or make one up, that's cool too): "
+    gets.chomp
+  end
 end
