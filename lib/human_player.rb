baf666fa169f9712662f1e2bb9364f8466a3da08
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-07-29 18:15:44 -0400

    Add display_available_colors method

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -7,4 +7,8 @@ class HumanPlayer < Player
     print "Please enter your name (or make one up, that's cool too): "
     gets.chomp
   end
+
+  def display_available_colors
+    puts AVAILABLE_COLORS.map { |color| color.to_s.colorize(color) }.join(' | ')
+  end
 end
