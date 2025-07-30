630b49316f916a277b916569e8a4dc722e29a623
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-07-30 10:18:09 -0400

    Add HumanPlayer guess_secret_code method

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -3,6 +3,17 @@
 require_relative 'player'
 
 class HumanPlayer < Player
+  def guess_secret_code
+    loop do
+      puts "\n#{@name}, guess the secret code, using the available colors (e.g. red green yellow blue):"
+      display_available_colors
+      guess = gets.chomp.downcase.split.map(&:to_sym)
+      return guess if guess.all? { |color| AVAILABLE_COLORS.include?(color) }
+    end
+  end
+
+  private
+
   def prompt_for_name
     print "Please enter your name (or make one up, that's cool too): "
     gets.chomp
