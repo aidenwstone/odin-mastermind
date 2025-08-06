488e9ec6e1ea61a2c1f6b123021f58015dffa740
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-08-06 17:07:23 -0400

    Allow user to play as code creator or guesser

--- main.rb
+++ main.rb
@@ -4,4 +4,15 @@ require_relative 'lib/game'
 require_relative 'lib/human_player'
 require_relative 'lib/computer_player'
 
-Game.new(ComputerPlayer, HumanPlayer).play
+puts 'Welcome to Mastermind, would you like to be the code creator or guesser? (c for creator, g for guesser):'
+answer = gets.chomp
+
+until %w[c g].include?(answer)
+  puts "\nPlease enter a valid response (c for creator, g for guesser):"
+  answer = gets.chomp
+end
+
+code_creator = answer == 'c' ? HumanPlayer : ComputerPlayer
+code_guesser = answer == 'g' ? HumanPlayer : ComputerPlayer
+
+Game.new(code_creator, code_guesser).play
