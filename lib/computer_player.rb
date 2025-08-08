477d51848828f400a19dc36b97d2f29f7f8c6061
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-08-08 17:19:21 -0400

    Add ComputerPlayer private keyword

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -5,11 +5,13 @@ require_relative 'player'
 class ComputerPlayer < Player
   POSSIBLE_NAMES = ['Moyer the Destroyer', 'Bad Chad', 'Baron von Cavitus', 'Dr. McFisto', 'Mr. Nemesis'].freeze
 
-  def prompt_for_name
-    POSSIBLE_NAMES.sample
-  end
-
   def create_secret_code
     AVAILABLE_COLORS.sample(4)
+  end
+
+  private
+
+  def prompt_for_name
+    POSSIBLE_NAMES.sample
   end
 end
